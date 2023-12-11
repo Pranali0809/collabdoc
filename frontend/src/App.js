03727be5429f60a7082f48719679ed442f48e855
Aarya Redekar <aaryamaya2002@gmail.com>
2023-12-11 18:55:34 +0530

    Add Doc Buttn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import {BrowserRouter as Router,Route} from "react-router-dom";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Home from './pages/Home'
 
 function App() {
   return (
+    <div>
     <Router>
-      <Route path="/" component={<Signin/>}/>
-      <Route path="/SignUp" component={<Signup/>}/>
-      <Route path="Home" component={Home}/>
+      <Routes>
+        <Route path="/" element={<Signin/>}/>
+        <Route path="/SignUp" element={<Signup/>}/>
+        <Route path="Home" element={<Home/>}/>
+      </Routes>
     </Router>
+    </div>
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import AddDocBut from '../components/AddDocBut'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='pl-4 pt-4 h-screen w-screen'>
+      <AddDocBut/>
     </div>
   )
 }
