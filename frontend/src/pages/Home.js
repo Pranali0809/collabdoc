f3d273c5fab83e8fa7c6afc274f9e5eba5d8ef7f
Aarya Redekar <aaryamaya2002@gmail.com>
2023-12-12 13:21:35 +0530

    Added Quill

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddDocBut from '../components/AddDocBut'
+import QuillSetup from '../components/QuillSetup'
 
 const Home = () => {
   return (
-    <div className='pl-4 pt-4 h-screen w-screen'>
+    <div className='p-4 h-screen w-screen'>
       <AddDocBut/>
+      <QuillSetup/>
     </div>
   )
 }
