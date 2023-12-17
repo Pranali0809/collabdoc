75decf9220f243f2b4399a329ccf85bd92466df9
Aarya Redekar <aaryamaya2002@gmail.com>
2023-12-17 16:45:17 +0530

    Changed Quill settings

--- backend/graphql/resolvers/auth.js
+++ backend/graphql/resolvers/auth.js
@@ -42,7 +42,6 @@ const assignCookies = (req, res) => {
             email,
             password: hashedPassword
           });
-          const result = await userMongoDB.save();
           await context.res.cookie('authToken', token, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000, 
